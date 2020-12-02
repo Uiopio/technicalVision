d400a22e312d4223cc300dbaea17e46e572a80b9
Ilya Lyaptsev <47566348+ilyalyai@users.noreply.github.com>
2020-12-02 14:04:02 +0300

    Файлы для 4 лабораторной

---
+++ ЛяпцевИА/Лаб4/Recognition.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <iostream>
+#include "opencv2/imgproc.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/core.hpp"
+#include <iostream>
+#include <conio.h>
+
+using namespace cv;
+using namespace std;
+
+void Task1(string fileName);
+
+void Task2(string fileName);
+
+void Task3(string fileName);
+
+void Task4();
